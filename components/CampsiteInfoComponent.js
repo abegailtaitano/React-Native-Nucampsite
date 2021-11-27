8c26e3f944418180ef05798926c7ecfe6661143a
Abegail Taitano <liagebat@gmail.com>
2021-11-27 11:10:40 -0800

    week-3-workshop-testingit

--- components/CampsiteInfoComponent.js
+++ components/CampsiteInfoComponent.js
@@ -26,6 +26,7 @@ function RenderCampsite(props) {
   const view = React.createRef();
 
   const recognizeDrag = ({dx}) => (dx < -200) ? true : false;
+  const recognizeComment = ({dx}) => (dx > 200) ? true : false;
 
   const panResponder = PanResponder.create({
       onStartShouldSetPanResponder: () => true,
@@ -53,9 +54,11 @@ function RenderCampsite(props) {
                   ],
                   { cancelable: false }
               );
+              }   else if(recognizeComment(gestureState)) {
+                props.onShowModal();
           }
           return true;
-      }
+      }, 
   });
 
   if (campsite) {
--- components/ReservationComponent.js
+++ components/ReservationComponent.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import { Text, View, ScrollView, StyleSheet,
-    Picker, Switch, Button, Modal  } from 'react-native';
+    Picker, Switch, Button, Alert } from 'react-native';
 import DateTimePicker from '@react-native-community/datetimepicker';
+import * as Animatable from 'react-native-animatable';
 
 class Reservation extends Component {
 
@@ -21,13 +22,31 @@ class Reservation extends Component {
         title: 'Reserve Campsite'
     }
     
-    toggleModal() {
+    /*toggleModal() {
         this.setState({showModal: !this.state.showModal});
-    }
+    } */
 
     handleReservation() {
         console.log(JSON.stringify(this.state));
-        this.toggleModal();
+       /* this.toggleModal();*/
+       Alert.alert(
+           "Begin Search?",
+           "Number of Campers: " + this.state.campers + "\n" + 
+           "Hike-In: " + this.state.hikeIn + "\n" +
+           "Date: " + this.state.date.toLocaleDateString('en-US'),
+           [
+               {
+                   text: "cancel",
+                   style: "cancel",
+                   onPress: () => console.log("Cancel Pressed")
+               },
+               {
+                   text: "OK",
+                   onPress: () => console.log("OK Pressed")
+               }
+           ],
+           { cancelable: false }
+       );
     }
 
     resetForm() {
@@ -36,11 +55,12 @@ class Reservation extends Component {
             hikeIn: false,
             date: new Date(),
             showCalendar: false,
-            showModal: false
+            //showModal: false
         });
     }
     
-    handleReservation() {
+   /* UNCOMMENT IF DOESNT WORK
+   handleReservation() {
         console.log(JSON.stringify(this.state));
         this.setState({
             campers: 1,
@@ -48,11 +68,15 @@ class Reservation extends Component {
             date: new Date(),
             showCalendar: false     
         });
-    }
+    }*/
 
     render() {
         return (
             <ScrollView>
+                <Animatable.View animation="zoomInUp"
+                    duration={2000}
+                    delay={1000}
+                    >
                 <View style={styles.formRow}>
                     <Text style={styles.formLabel}>Number of Campers</Text>
                     <Picker
@@ -107,33 +131,7 @@ class Reservation extends Component {
                         accessibilityLabel='Tap me to search for available campsites to reserve'
                     />
                 </View>
-                <Modal
-                    animationType={'slide'}
-                    transparent={false}
-                    visible={this.state.showModal}
-                    onRequestClose={() => this.toggleModal()}
-                >
-                    <View style={styles.modal}>
-                        <Text style={styles.modalTitle}>Search Campsite Reservations</Text>
-                        <Text style={styles.modalText}>
-                            Number of Campers: {this.state.campers}
-                        </Text>
-                        <Text style={styles.modalText}>
-                            Hike-In?: {this.state.hikeIn ? 'Yes' : 'No'}
-                        </Text>
-                        <Text style={styles.modalText}>
-                            Date: {this.state.date.toLocaleDateString('en-US')}
-                        </Text>
-                        <Button
-                            onPress={() => {
-                                this.toggleModal();
-                                this.resetForm();
-                            }}
-                            color='#5637DD'
-                            title='Close'
-                        />
-                    </View>
-                </Modal>
+                </Animatable.View>
             </ScrollView>
         );
     }
